568dbeb4c1ed93cbde8310452a19c34e5c53764f
Afis <mahoutinafis@gmail.com>
2022-11-08 04:43:06 +0100

    Correct class format output

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel:
         Called when a BaseModel object is created
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.update_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
     def __str__(self):
         """
@@ -33,12 +33,14 @@ class BaseModel:
         Updates the public instance attribute
         update_at with the current datetime
         """
-        self.update_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.update_at = datetime.now()
 
     def to_dict(self):
         """
         Returns a dictionary containing all keys/values of dict
         """
         temp = self.__dict__
+        temp['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        temp['update_at'] = self.update_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         temp['__class__'] = self.__class__.__name__
         return temp
