97021dd9e6dd6f7ad25189a2d276f54ebb080965
Afis <mahoutinafis@gmail.com>
2022-11-08 04:12:16 +0100

    Create Base Model

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!usr/bin/python3
+"""
+This module contains Class Base model
+That defines all common attributes and Methods for other
+Classes
+"""
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """
+    Defines all common attributes/methods for other classes
+    """
+
+    def __init__(self):
+        """
+        Called when a BaseModel object is created
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.update_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+    def __str__(self):
+        """
+        String representation of Base Model
+        """
+        return "[" + self.__class__.__name__ +
+        "] (<" + self.id + ">) <" + str(self.__dict__) + ">"
+
+    def save(self):
+        """
+        Updates the public instance attribute
+        update_at with the current datetime
+        """
+        self.update_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of dict
+        """
+        temp = self.__dict__
+        temp['__class__'] = self.__class__.__name__
+        return temp
