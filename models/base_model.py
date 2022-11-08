1d0b10ea0249318c9b7da58d012e909b4127f2a4
Afis <mahoutinafis@gmail.com>
2022-11-08 20:44:45 +0100

    Update tests

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.update_at = self.created_at
 
     def __str__(self):
         """
