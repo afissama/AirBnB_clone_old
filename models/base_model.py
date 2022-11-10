c32d8d20d3a22f40682b146dede82c52a7202749
Afis <mahoutinafis@gmail.com>
2022-11-10 06:04:20 +0100

    Model from dicr

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,21 @@ class BaseModel:
     Defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Called when a BaseModel object is created
         """
+
+        if len(kwargs):
+            for key, value in kwargs.items():
+                _val = value
+                if key == "__class__":
+                    continue
+
+                if (key in ["created_at", "updated_at"]):
+                    _val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, _val)
+            return
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = copy.deepcopy(self.created_at)
