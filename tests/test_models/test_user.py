1aadbebc660886e8cc84b298efc77676c5ec26c6
Afis <mahoutinafis@gmail.com>
2022-11-11 15:16:34 +0100

    User test

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to test User model
+"""
+from models.base_model import BaseModel
+from models.user import User
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    """Test User Attributes
+    """
+
+    def test_mail(self):
+        user = User()
+        user.email = "my@enemy.com"
+        self.assertEqual(user.email, "my@enemy.com")
