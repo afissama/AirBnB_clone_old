b7185c62a66b18c9f96722cfc611347ea7d8ca00
Afis <mahoutinafis@gmail.com>
2022-11-12 07:20:58 +0100

    update tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,6 +4,7 @@
 from models.base_model import BaseModel
 from models.user import User
 import unittest
+import models
 
 
 class TestUser(unittest.TestCase):
@@ -27,3 +28,24 @@ class TestUser(unittest.TestCase):
         self.assertEqual(user.password, User.password)
         self.assertEqual(str, type(User.password))
         self.assertIsNotNone(User.password)
+
+    def test_firstname(self):
+        """
+        Test first name 
+        attribue
+        """
+        my_user = User()
+        my_user.first_name = "Betty"
+        my_user.last_name = "Bar"
+        my_user.email = "airbnb@mail.com"
+        my_user.password = "root"  
+        my_user.save()
+
+        all_objs = models.storage.all()
+        
+        user_id = "User.{}".format(my_user.id)
+        loaded_user = all_objs[user_id]
+
+        self.assertIn(user_id, all_objs.keys())
+        self.assertEqual(my_user.first_name, loaded_user.first_name)
+        self.assertEqual(my_user.last_name, loaded_user.last_name)
