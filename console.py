a31b91b2b488f5b41538d7911f554363d38cb8c2
Afis <mahoutinafis@gmail.com>
2022-11-12 09:19:33 +0100

    update console

--- console.py
+++ console.py
@@ -7,7 +7,11 @@ import sys
 import models
 from models.base_model import BaseModel
 from models.user import User
-import inspect
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
