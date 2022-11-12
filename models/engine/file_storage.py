bf41bdba80d580f77c79536a10d31b4f0c6c747d
Afis <mahoutinafis@gmail.com>
2022-11-12 08:32:25 +0100

    update filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@ file and deserializes JSON file to instances.
 """
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import os
 import json
 
